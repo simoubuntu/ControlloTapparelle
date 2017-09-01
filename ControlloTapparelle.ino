008789e85b41318971cb9f48af9f7978f1c18224
Simone Bosotti <simone.bosotti@outlook.com>
2017-09-01 17:36:15 +0200

    Acquisizione valori sensori e trasmissione seriale

--- ControlloTapparelle.ino
+++ ControlloTapparelle.ino
@@ -6,11 +6,19 @@ int sensor2Val = 0;
 
 void setup()
 {
-
-
+	Serial.begin(9600);
+	Serial.println("Serial Communication Initialized");
 }
 
 void loop()
 {
+	sensor1Val = analogRead(sensor1Pin);
+	sensor2Val = analogRead(sensor2Pin);
 
+	Serial.print("S1 = ");
+	Serial.print(sensor1Val);
+	Serial.print(" - S2 = ");
+	Serial.println(sensor2Val);
+
+	delay(1000);
 }
